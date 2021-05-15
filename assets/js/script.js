a5d962df23a9224d7c29c413adfbca47cc0fd426
Bruno Aguirre <bruno_aguirrejr@yahoo.com>
2021-05-15 14:54:50 -0700

    created event listeners and coded timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,90 @@
+let score = 0;
+let timeLeft = 50;
+const timePenalty = -5;
+
+// create array for questions & answers
 var quizQuestions = [
-	{
-		question: "Which event occurs when the user clicks on an HTML element?",
-		answers: {
-			a: 'onmouseover',
-			b: 'onclick',
-			c: 'onmouseclick',
-            d: 'onchange'
-		},
-		correctAnswer: 'b'
-	},
-	{
-		question: "Which operator is used to assign a value to a variable?",
-		answers: {
-			a: '*',
-			b: '-',
-			c: '=',
-            d: 'x'
-		},
-		correctAnswer: 'c'
-	},
     {
-		question: "How do you round the number 8.15 to the nearest integer?",
-		answers: {
-			a: 'round(8.15)',
-			b: 'Math.round(8.15)',
-			c: 'Math.rnd(8.15)',
-            d: 'rnd(8.15)'
-		},
-		correctAnswer: 'b'
-	},
-	{
-		question: "How to write an IF statement in JavaScript?",
-		answers: {
-			a: 'if i = 7',
-			b: 'if i = 7 then',
-			c: 'if i == 7 then',
-            d: 'if (i == 7)'
-		},
-		correctAnswer: 'd'
-	},
+        question: "Which event occurs when the user clicks on an HTML element?",
+        a: 'onmouseover',
+        b: 'onclick',
+        c: 'onmouseclick',
+        d: 'onchange',
+        answer: 'b'
+    },
     {
-		question: "Inside which HTML element do we put the JavaScript?",
-		answers: {
-			a: '<script>',
-			b: '<js>',
-			c: '<javascript>',
-            d: '<scripting>'
-		},
-		correctAnswer: 'a'
-	},
-	{
-		question: "JavaScript is the same as Java.",
-		answers: {
-			a: 'true',
-			b: 'false',
-		},
-		correctAnswer: 'b'
-	},
+        question: "Which operator is used to assign a value to a variable?",
+            a: '*',
+            b: '-',
+            c: '=',
+            d: 'x',
+        answer: 'c'
+    },
     {
-		question: "How do you create a function in JavaScript?",
-		answers: {
-			a: 'myFunction function{}',
-			b: 'function:myFunction()',
-			c: 'function = myFunction()',
-            d: 'function myFunction()'
-		},
-		correctAnswer: 'd'
-	},
-	{
-		question: "JavaScript is not case-sensitive?",
-		answers: {
-			a: 'true',
-			b: 'false',
-		},
-		correctAnswer: 'b'
-	}
+        question: "How do you round the number 8.15 to the nearest integer?",
+            a: 'round(8.15)',
+            b: 'Math.round(8.15)',
+            c: 'Math.rnd(8.15)',
+            d: 'rnd(8.15)',
+        answer: 'b'
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+            a: 'if i = 7',
+            b: 'if i = 7 then',
+            c: 'if i == 7 then',
+            d: 'if (i == 7)',
+        answer: 'd'
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+            a: '<script>',
+            b: '<js>',
+            c: '<javascript>',
+            d: '<scripting>',
+        answer: 'a'
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+            a: 'myFunction function{}',
+            b: 'function:myFunction()',
+            c: 'function = myFunction()',
+            d: 'function myFunction()',
+        answer: 'd'
+    },
 ];
+let startQuizEl = document.querySelector("#start-btn");
+let containerEl = document.querySelector(".container");
+let modalEl = document.querySelector(".modal");
+let timerEl = document.querySelector("#timer");
+
+function countdowmTimer() {
+    let timer = setInterval(function () {
+       timerEl.textContent = timeLeft;
+        if (timeLeft > 1) {
+            timeLeft--;
+        } else {
+            clearInterval(timer);
+            alert('Game Over!');
+        }
+    }, 1000);
+}
+
+
+function showQuestion() {
+    quizQuestions.forEach(element => console.log(element.question));
+}
+
+
+function startQuiz() {
+    modalEl.setAttribute("class", "hide")
+    containerEl.removeAttribute("class", "hide")
+    countdowmTimer();
+    showQuestion();
+
+}
 
 
 
-
-let buttonEl = document.querySelector(".start-btn");
-
-buttonEl.addEventListener("click", countdownTimer);
+// starts coding quiz
+startQuizEl.addEventListener("click", startQuiz);
